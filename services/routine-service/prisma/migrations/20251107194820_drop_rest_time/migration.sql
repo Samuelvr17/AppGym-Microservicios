33c7823c131dbd9bafec4b2a301c575296184005
Samuel Vergara <samuelvergarar@gmail.com>
2025-11-07 14:51:41 -0500

    migrations

---
+++ services/routine-service/prisma/migrations/20251107194820_drop_rest_time/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `rest_time` on the `routine_exercises` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "routine_exercises" DROP COLUMN "rest_time";
---
+++ services/workout-service/prisma/migrations/20251107194840_drop_rest_time/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `rest_time` on the `workout_sets` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "workout_sets" DROP COLUMN "rest_time";
